03a50798608f1ca484f9f94d753b058678c2f49f
Sunil <sunilbennur25@gmail.com>
2023-08-03 00:14:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,25 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1" # Replace with your desired region
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16" # Replace with your desired VPC CIDR block
+}
+
+resource "aws_subnet" "public" {
+  count             = 5
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index}.0/24" # Replace with your desired public subnet CIDR blocks
+  availability_zone = "us-east-1a"               # Replace with your desired availability zone
+}
+
+resource "aws_instance" "example" {
+  count         = 5
+  ami           = "ami-053b0d53c279acc90" # Replace with your desired AMI ID
+  instance_type = "t2.micro"              # Replace with your desired instance type
+  subnet_id     = aws_subnet.public[count.index].id
   tags = {
-      Name = "TF-Instance"
+    Name = "Instance-${count.index + 1}"  # Unique name for each instance
   }
+
 }
